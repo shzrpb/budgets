d65156630d96205258a053f977304f52f35aaa84
shzrpb <sherylgoh95@gmail.com>
2026-07-30 23:36:25 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,6 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
-  viewportFit: "cover",
-  themeColor: "#faf9f7",
 };
 
 export default function RootLayout({
